43f50543d4e72956e0f05b038fa528c762317158
Zohaib Azam <66849463+Zohaib58@users.noreply.github.com>
2024-11-18 11:33:01 +0500

    File Uploading Successful

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
-@Controller()
+@Controller({ path: 'bl4c', version: '1' }) 
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post("/result")
+  @UseInterceptors(FileInterceptor('file'))
+  handleExcelUpload(@UploadedFile() file: Express.Multer.File): string {
+    console.log(file.originalname); // File name
+    console.log(file.mimetype); // MIME type
+    console.log(file.buffer); // File buffer (binary data)
+
+    return `File ${file.originalname} uploaded successfully!`;
   }
 }
